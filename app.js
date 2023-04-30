d0fcd8c930d64e8f4e6b2941f55667c0f29a2d87
samar897 <samar.abdullrazzag@hotmail.com>
2023-04-30 14:14:47 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+//here we will pring the lib for express  will pring back all the opj
+
+//step 2 to call the opj
+
+const app = express();
+
+//أحدى الدوال هي liseane فتحت منفذ عشان يسمع من خلال الكمبيوتر
+/*
+
+الفعل او الطلب والثاني ردة الفعل 
+
+*/
+
+//Step 4
+
+app.get(
+  "/",
+
+  function (req, res) {
+    res.send("welcome to the javascript bootcamp");
+  }
+);
+
+app.get("/hi", function (req, res) {
+  res.send("Welcome to Tuwaiq");
+});
+
+app.get("/name", function (req, res) {
+  res.send("welcome to the javascript bootcamp Samar");
+});
+
+//STEP3
+app.listen(8000, function () {
+  console.log("listening");
+});
+
+//npx nodemon app.js to run with the update
