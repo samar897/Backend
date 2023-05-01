ffa0e28582542db85ba4583d576ba5970bb7743a
samar897 <samar.abdullrazzag@hotmail.com>
2023-05-01 13:17:45 +0300

    The Second Lab
    The Lab with Adding the EJS Eng with HTML File.

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require("express");
 
 const app = express();
 
+
+
 //أحدى الدوال هي liseane فتحت منفذ عشان يسمع من خلال الكمبيوتر
 /*
 
@@ -16,6 +18,8 @@ const app = express();
 
 */
 
+app.set('view engine', 'ejs');
+
 //Step 4
 
 app.get(
@@ -23,20 +27,25 @@ app.get(
 
   function (req, res) {
     res.send("welcome to the javascript bootcamp");
+    
   }
 );
 
-app.get("/hi", function (req, res) {
-  res.send("Welcome to Tuwaiq Samar Saleh");
-});
+app.get("/html", function (req, res) {
+ // res.send("Welcome to Tuwaiq Samar Saleh");
 
+  const Result = 100;
+    res.render("main.ejs",{Result2: Result});
+
+});
+/*
 app.get("/name", function (req, res) {
   res.send("welcome to the javascript bootcamp Samar");
-});
+});*/
 
 //STEP3
 app.listen(8000, function () {
   console.log("listening");
 });
 
-//npx nodemon app.js to run with the update
+//npx nodemon app.js to run with the update.
